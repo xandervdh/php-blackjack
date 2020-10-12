c6680288ae8a1ddab72adec242b078a2d43ad5f6
xandervdh <xander-v.d.h@hotmail.com>
2020-10-12 14:21:20 +0200

    changed example to index and added error handling

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require 'classes/Suit.php';
+require 'classes/Card.php';
+require 'classes/Deck.php';
+
+$deck = new Deck();
+$deck->shuffle();
+foreach($deck->getCards() AS $card) {
+    echo $card->getUnicodeCharacter(true);
+    echo '<br>';
+}
